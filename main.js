3648735dd10430e53b3ce00955cdf877113b5c36
Moreene <moreenewu@gmail.com>
2023-08-02 22:30:25 +0800

    優化程式碼

--- main.js
+++ main.js
@@ -14,3 +14,28 @@ function toggleMenu() {
 
 hamBtn.addEventListener('click', toggleMenu);
 hamCloseBtn.addEventListener("click", toggleMenu);
+
+// topbar 線上預約 active(無法透過currentPage來增加active樣式的部分)
+document.addEventListener("DOMContentLoaded", function () {
+    let currentUrl = window.location.href;
+    let loginLink = document.getElementById("loginLink");
+    let reverseLink = document.getElementById("reverseLink");
+
+
+    let signUpHref = "http://localhost:5173/HayMo-Dog-Hotel/pages/signUp.html";
+    let reverseCosmeticHref = "http://localhost:5173/HayMo-Dog-Hotel/pages/reverse-cosmetic.html";
+    let reverseStayHref = "http://localhost:5173/HayMo-Dog-Hotel/pages/reverse-stay.html";
+
+    switch (currentUrl) {
+        case signUpHref:
+            loginLink.classList.add("active");
+            break;
+        case reverseCosmeticHref:
+            reverseLink.classList.add("active");
+            break;
+        case reverseStayHref:
+            reverseLink.classList.add("active");
+            break;
+    }
+});
+
